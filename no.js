93a4bac8b240fb4972fa95bd2407dd5f1cb33002
ThunderRedStar <wvongphanith30@stuy.edu>
2022-02-08 20:59:09 -0500

    autocommit on change

--- no.js
+++ no.js
@@ -2,6 +2,14 @@ const fs = require(
     'fs'
 )
 
-const sleep = () => {
-
+const sleep = (ms) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve()
+        }, ms)
+    })
 }
+
+(async () => {
+
+})
